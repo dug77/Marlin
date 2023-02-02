3483e417aea9d7645fed7e7b3543a74d05fc5b5b
dug77 <46523737+dug77@users.noreply.github.com>
2023-02-02 01:47:18 +0000

    add auto versioning

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -60,7 +60,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Douglas McKinnon " __DATE__ " " __TIME__ ")" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(DM " AUTO_VERSION " "  __DATE__ " " __TIME__ ")" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
