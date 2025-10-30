f71ac4ece1b72f38e1766bfe8790bbce62a63e44
dug77 <46523737+dug77@users.noreply.github.com>
2025-10-30 14:43:38 +0000

    Adjust temperature settings: reduce residency time to 10 seconds and increase hysteresis to 4°C

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -562,9 +562,9 @@
 #endif
 
 #if HAS_E_TEMP_SENSOR
-  #define TEMP_RESIDENCY_TIME         20  // (seconds) Time to wait for hotend to "settle" in M109
+  #define TEMP_RESIDENCY_TIME         10  // (seconds) Time to wait for hotend to "settle" in M109
   #define TEMP_WINDOW                  1  // (°C) Temperature proximity for the "temperature reached" timer
-  #define TEMP_HYSTERESIS              3  // (°C) Temperature proximity considered "close enough" to the target
+  #define TEMP_HYSTERESIS              4  // (°C) Temperature proximity considered "close enough" to the target
 #endif
 
 #if TEMP_SENSOR_BED
